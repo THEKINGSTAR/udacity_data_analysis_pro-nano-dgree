a32a5ce455c7f3a66c6d39fbcc1439982b497964
K <thekingstar5002@gmail.com>
2021-06-18 02:40:53 +0200

    FINSHED THE NONO DEGREE AND THE FINAL PROJECT

---
+++ 3. Introduction to Data Analysis/Lesson 3 THE Data Analysis Process/27 - Communicating Results Practice.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Mar  7 10:47:48 2021
+
+@author: KING
+"""
+
+import numpy as np
+import  pandas as pd
+import  matplotlib as plt
+
+df_stores = pd.read_csv("store_data.csv")
+print(df_stores)
+
